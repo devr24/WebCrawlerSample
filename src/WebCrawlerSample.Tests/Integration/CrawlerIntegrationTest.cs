ce53af1e0803a4bfdf7c819fee3c91c8e478303e
Rob <rmccabe@eshopworld.com>
2021-04-13 22:29:31 +0100

    Added Integration test

---
+++ src/WebCrawlerSample.Tests/Integration/CrawlerIntegrationTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using System.Threading.Tasks;
+using WebCrawlerSample.Services;
+using Xunit;
+
+namespace WebCrawlerSample.Tests.Unit
+{
+    public class CrawlerIntegrationTest
+    {
+        // Run the crawler to one depth of the test page and ensure the results are as expected.
+        [Fact]
+        public async Task Test_Crawler_RunAsync()
+        {
+            // Arrange
+            var testSite = "https://www.crawler-test.com/";
+            var crawler = new WebCrawer(new Downloader(), new HtmlParser());
+            
+            // Act
+            var result = await crawler.RunAsync(testSite);
+
+            // Assert
+            result.MaxDepth.Should().Be(1);
+            result.RunTime.Should().BeGreaterThan(new TimeSpan());
+            result.Site.Should().BeEquivalentTo(new Uri(testSite));
+            result.Links.Count.Should().Be(1); // hard coded expected links - not ideal.
+            result.Links.FirstOrDefault().Value.PageLinks.Count.Should().Be(412); // hard coded expected links - not ideal.
+        }
+    }
+}
