c3d69790bac67ae62a3310776ab88667d7035345
Rob <6367545+devr24@users.noreply.github.com>
2025-07-09 15:34:49 +0100

    Refactor crawler into reusable library

--- src/WebCrawlerSample.Tests/Unit/CrawlerUnitTest.cs
+++ src/WebCrawlerSample.Tests/Unit/CrawlerUnitTest.cs
@@ -9,8 +9,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using Moq;
 using WebCrawlerSample.Tests;
-using WebCrawlerSample.Services;
-using WebCrawlerSample.Models;
+using WebCrawler.Core.Services;
+using WebCrawler.Core.Models;
 using Xunit;
 
 namespace WebCrawlerSample.Tests.Unit
